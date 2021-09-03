5a6e644d8bbda8d4e010b5f10d3014618327194e
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2021-09-03 18:00:57 -0300

    first-steps

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,5 +1,35 @@
 <script>
-	export let name;
+	import ContactCard from './ContactCard.svelte';
+
+
+	let name = 'Ravi';
+	let age = 12;
+
+	let job = '';
+	let description = '';
+	let image = '';
+
+	$: uppercaseName = name.toUpperCase();
+
+	$: if (name === 'Isis'){
+		age = 31;
+	}
+
+	function incrementAge(){
+		age += 1;
+	}
+
+	function changeName(){
+		name = 'Isis';
+	}
+
+	function nameInput(event){
+		const enteredValue = event.target.value;
+		name = enteredValue;
+		
+	}
+
+
 </script>
 
 <style>
@@ -8,4 +38,18 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello! My name is {uppercaseName}, and my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
+<input type="text" bind:value="{name}"> 
+<input type="text" bind:value="{job}"> 
+<input type="text" bind:value="{image}"> 
+<textarea rows="3" bind:value="{description}"></textarea>
+
+
+<ContactCard 
+	userName="{name}" 
+	jobTitle="{job}" 
+	{description}
+	userImage="{image}"/>
